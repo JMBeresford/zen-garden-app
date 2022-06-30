42779eeef6bcf61d81a96db714fe7bb1a4606bc1
John Beresford <jberesford@volcaus.com>
2022-06-29 22:33:22 -0700

    log added :sparkles: prior to new sand

--- src/components/canvas/Garden.jsx
+++ src/components/canvas/Garden.jsx
@@ -3,8 +3,9 @@ import { useMemo, useRef } from 'react';
 import Tile from './Tile';
 import { useEffect } from 'react';
 import { Perf } from 'r3f-perf';
-import { OrbitControls } from '@react-three/drei';
+import { Instances, OrbitControls } from '@react-three/drei';
 import { useControls } from 'leva';
+import Log from './Log';
 
 const Garden = ({ ...props }) => {
   const ref = useRef();
@@ -32,9 +33,37 @@ const Garden = ({ ...props }) => {
     });
   }, [gridSize, tiles]);
 
+  const logs = useMemo(() => {
+    let mid = Math.floor(gridSize / 2);
+    let logs = [];
+    let y = 0.05;
+
+    for (let i = 0; i < gridSize; i++) {
+      logs.push(
+        // top
+        <Log
+          position={[-mid + i, y, -mid - 0.5]}
+          rotation={[0, Math.PI / 2, 0]}
+        />,
+        // bottom
+        <Log
+          position={[-mid + i, y, mid + 0.5]}
+          rotation={[0, Math.PI / 2, 0]}
+        />,
+        // left
+        <Log position={[-mid - 0.5, y, -mid + i]} />,
+        // right
+        <Log position={[mid + 0.5, y, -mid + i]} />
+      );
+    }
+
+    return logs;
+  }, [gridSize]);
+
   return (
     <>
       <group ref={ref} {...props}>
+        <Instances ma></Instances>
         {garden.map((tile, index) => (
           <Tile
             key={index}
@@ -46,6 +75,7 @@ const Garden = ({ ...props }) => {
           />
         ))}
       </group>
+      {logs}
 
       <pointLight intensity={0.15} position={sunPos} color={sunColor}>
         <mesh>
@@ -54,7 +84,6 @@ const Garden = ({ ...props }) => {
         </mesh>
       </pointLight>
       <ambientLight intensity={0.25} />
-
       <OrbitControls />
       {debug && <Perf position='bottom-left' />}
     </>
